50edbcb257e9074e97b06b1def59e6541fae84c0
Lodder <lodder15@hotmail.co.uk>
2018-06-19 16:34:25 +0100

    Create watch.js

---
+++ watch.js
@@ -0,0 +1,35 @@
+// Require and definitions
+const chalk    = require('chalk');
+const chokidar = require('chokidar');
+const exec     = require('child_process').exec;
+const log      = console.log;
+
+// Config
+const dir    = 'C:/dev/new/capture_pages/webapp';
+const script = '"move.sh"';
+
+// Initialize watcher.
+const watcher = chokidar.watch(dir, {
+	ignored: /[\/\\]\./,
+	persistent: true
+});
+
+// Define events and tasks
+watcher
+	.on('ready', (path) => {
+		log(chalk.yellow.bold('Watching...'));
+	})
+	.on('change', (path) => {
+		log('File ' + chalk.cyan.bold(path) + ' has been changed');
+
+		exec(script, (error, stdout, stderr) => {
+			if (error) {
+				log(chalk.red.bold('There was an error upoading files to VM!'));
+				return;
+			}
+
+			log(chalk.green.bold('Files successfully uploaded to VM!'));
+			log(chalk.yellow.bold('Watching...'));
+		});
+	});
+	
